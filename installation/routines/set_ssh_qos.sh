764c9671bcd0673aaf42cacfb6c8cfaaa3f2f9a3
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2023-12-04 23:32:50 +0100

    make ssh qos rerunnable

--- installation/routines/set_ssh_qos.sh
+++ installation/routines/set_ssh_qos.sh
@@ -2,10 +2,23 @@
 
 set_ssh_qos() {
     if [ "$DISABLE_SSH_QOS" == true ] ; then
+        local sshd_config="/etc/ssh/sshd_config"
+        local ssh_config="/etc/ssh/ssh_config"
+        local ssh_qos_entry="IPQoS 0x00 0x00\n"
+
         # The latest version of SSH installed on the Raspberry Pi 3 uses QoS headers, which disagrees with some
         # routers and other hardware. This causes immense delays when remotely accessing the RPi over ssh.
         log "  Set SSH QoS to best effort"
-        echo -e "IPQoS 0x00 0x00\n" | sudo tee -a /etc/ssh/sshd_config
-        echo -e "IPQoS 0x00 0x00\n" | sudo tee -a /etc/ssh/ssh_config
+        if grep -qiw "${ssh_qos_entry}" "${sshd_config}"; then
+            log "    Skipping ${sshd_config}. Already set up!"
+        else
+            echo -e "${ssh_qos_entry}" | sudo tee -a "${sshd_config}"
+        fi
+
+        if grep -qiw "${ssh_qos_entry}" "${ssh_config}"; then
+            log "    Skipping ${sshd_config}. Already set up!"
+        else
+            echo -e "${ssh_qos_entry}" | sudo tee -a "${ssh_config}"
+        fi
     fi
 }
