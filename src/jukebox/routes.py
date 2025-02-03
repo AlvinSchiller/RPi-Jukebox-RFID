ae4dad7f1b32a05864c757ad6a7a9c9edf7c36b6
Alvin Schiller <103769832+AlvinSchiller@users.noreply.github.com>
2025-02-03 01:17:12 +0100

    test: add new finding

--- src/jukebox/routes.py
+++ src/jukebox/routes.py
@@ -10,6 +10,7 @@ def index():
     name = request.args.get('name')
     author = request.args.get('author')
     read = bool(request.args.get('read'))
+    test = bool(request.args.get('test'))
 
     if name:
         cursor.execute(
